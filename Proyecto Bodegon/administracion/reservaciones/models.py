82130af81e004f6825e61490ac39f9b399b75187
Barcelo Emmanuel <emmanuelbarcelo22@gmail.com>
2025-03-24 18:12:16 -0300

    Proyecto finalizado

--- Proyecto Bodegon/administracion/reservaciones/models.py
+++ Proyecto Bodegon/administracion/reservaciones/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Reservas(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length = 255)
-    cantidad_invitados = models.IntegerField(6)
+    cantidad_invitados = models.IntegerField()
     fecha_reserva = models.DateTimeField()
 
 
@@ -11,4 +11,4 @@ class Menus(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length = 255)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    inventario = models.IntegerField(5)
+    inventario = models.IntegerField()
